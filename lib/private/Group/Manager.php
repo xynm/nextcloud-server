ee0c49f7988b048e83cc3e83b44d87603d217279
Joas Schilling <coding@schilljs.com>
2019-11-19 16:43:46 +0100

    Fix autocomplete suggestions with numeric user ids
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/private/Group/Manager.php
+++ lib/private/Group/Manager.php
@@ -385,7 +385,7 @@ class Manager extends PublicEmitter implements IGroupManager {
 
 		$matchingUsers = [];
 		foreach($groupUsers as $groupUser) {
-			$matchingUsers[$groupUser->getUID()] = $groupUser->getDisplayName();
+			$matchingUsers[(string) $groupUser->getUID()] = $groupUser->getDisplayName();
 		}
 		return $matchingUsers;
 	}
