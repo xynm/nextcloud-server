ykcab/server

Direct editing API to allow file editing using a one-time token for mobile apps
Signed-off-by: Julius Härtl <jus@bitgrid.net>

lib/composer/composer/autoload_static.php
@@ -198,6 +198,13 @@ class ComposerStaticInit53792487c5a8370acc0b06b1a864ff4c
         'OCP\\Diagnostics\\IEventLogger' => __DIR__ . '/../../..' . '/lib/public/Diagnostics/IEventLogger.php',
         'OCP\\Diagnostics\\IQuery' => __DIR__ . '/../../..' . '/lib/public/Diagnostics/IQuery.php',
         'OCP\\Diagnostics\\IQueryLogger' => __DIR__ . '/../../..' . '/lib/public/Diagnostics/IQueryLogger.php',
+        'OCP\\DirectEditing\\ICreateEmpty' => __DIR__ . '/../../..' . '/lib/public/DirectEditing/ICreateEmpty.php',
+        'OCP\\DirectEditing\\ICreateFromTemplate' => __DIR__ . '/../../..' . '/lib/public/DirectEditing/ICreateFromTemplate.php',
+        'OCP\\DirectEditing\\IEditor' => __DIR__ . '/../../..' . '/lib/public/DirectEditing/IEditor.php',
+        'OCP\\DirectEditing\\IManager' => __DIR__ . '/../../..' . '/lib/public/DirectEditing/IManager.php',
+        'OCP\\DirectEditing\\ITemplate' => __DIR__ . '/../../..' . '/lib/public/DirectEditing/ITemplate.php',
+        'OCP\\DirectEditing\\IToken' => __DIR__ . '/../../..' . '/lib/public/DirectEditing/IToken.php',
+        'OCP\\DirectEditing\\RegisterDirectEditorEvent' => __DIR__ . '/../../..' . '/lib/public/DirectEditing/RegisterDirectEditorEvent.php',
         'OCP\\Encryption\\Exceptions\\GenericEncryptionException' => __DIR__ . '/../../..' . '/lib/public/Encryption/Exceptions/GenericEncryptionException.php',
         'OCP\\Encryption\\IEncryptionModule' => __DIR__ . '/../../..' . '/lib/public/Encryption/IEncryptionModule.php',
         'OCP\\Encryption\\IFile' => __DIR__ . '/../../..' . '/lib/public/Encryption/IFile.php',
@@ -808,6 +815,7 @@ class ComposerStaticInit53792487c5a8370acc0b06b1a864ff4c
         'OC\\Core\\Migrations\\Version16000Date20190428150708' => __DIR__ . '/../../..' . '/core/Migrations/Version16000Date20190428150708.php',
         'OC\\Core\\Migrations\\Version17000Date20190514105811' => __DIR__ . '/../../..' . '/core/Migrations/Version17000Date20190514105811.php',
         'OC\\Core\\Migrations\\Version18000Date20190920085628' => __DIR__ . '/../../..' . '/core/Migrations/Version18000Date20190920085628.php',
+        'OC\\Core\\Migrations\\Version18000Date20191014105105' => __DIR__ . '/../../..' . '/core/Migrations/Version18000Date20191014105105.php',
         'OC\\Core\\Notification\\RemoveLinkSharesNotifier' => __DIR__ . '/../../..' . '/core/Notification/RemoveLinkSharesNotifier.php',
         'OC\\Core\\Service\\LoginFlowV2Service' => __DIR__ . '/../../..' . '/core/Service/LoginFlowV2Service.php',
         'OC\\DB\\Adapter' => __DIR__ . '/../../..' . '/lib/private/DB/Adapter.php',
@@ -859,6 +867,8 @@ class ComposerStaticInit53792487c5a8370acc0b06b1a864ff4c
         'OC\\Diagnostics\\EventLogger' => __DIR__ . '/../../..' . '/lib/private/Diagnostics/EventLogger.php',
         'OC\\Diagnostics\\Query' => __DIR__ . '/../../..' . '/lib/private/Diagnostics/Query.php',
         'OC\\Diagnostics\\QueryLogger' => __DIR__ . '/../../..' . '/lib/private/Diagnostics/QueryLogger.php',
+        'OC\\DirectEditing\\Manager' => __DIR__ . '/../../..' . '/lib/private/DirectEditing/Manager.php',
+        'OC\\DirectEditing\\Token' => __DIR__ . '/../../..' . '/lib/private/DirectEditing/Token.php',
         'OC\\Encryption\\DecryptAll' => __DIR__ . '/../../..' . '/lib/private/Encryption/DecryptAll.php',
         'OC\\Encryption\\EncryptionWrapper' => __DIR__ . '/../../..' . '/lib/private/Encryption/EncryptionWrapper.php',
         'OC\\Encryption\\Exceptions\\DecryptionFailedException' => __DIR__ . '/../../..' . '/lib/private/Encryption/Exceptions/DecryptionFailedException.php',

lib/private/DirectEditing/Manager.php
@@ -1,0 +1,232 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019 Julius Härtl <jus@bitgrid.net>
+ *
+ * @author Julius Härtl <jus@bitgrid.net>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OC\DirectEditing;
+
+use Doctrine\DBAL\FetchMode;
+use OCP\AppFramework\Http\NotFoundResponse;
+use OCP\AppFramework\Http\Response;
+use OCP\AppFramework\Http\TemplateResponse;
+use OCP\DB\QueryBuilder\IQueryBuilder;
+use OCP\DirectEditing\ICreateFromTemplate;
+use OCP\DirectEditing\IEditor;
+use \OCP\DirectEditing\IManager;
+use OCP\DirectEditing\IToken;
+use OCP\Files\File;
+use OCP\Files\IRootFolder;
+use OCP\Files\NotFoundException;
+use OCP\IDBConnection;
+use OCP\IUserSession;
+use OCP\Security\ISecureRandom;
+use OCP\Share\IShare;
+
+class Manager implements IManager {
+
+	/** @var IEditor[] */
+	private $editors;
+
+	public const TABLE_TOKENS = 'direct_edit';
+
+	/** @var IDBConnection */
+	private $connection;
+	/**
+	 * @var ISecureRandom
+	 */
+	private $random;
+	private $userId;
+	private $rootFolder;
+
+	public function __construct(
+		ISecureRandom $random,
+		IDBConnection $connection,
+		IUserSession $userSession,
+		IRootFolder $rootFolder
+	) {
+		$this->random = $random;
+		$this->connection = $connection;
+		$this->userId = $userSession->getUser() ? $userSession->getUser()->getUID() : null;
+		$this->rootFolder = $rootFolder;
+	}
+
+	public function registerDirectEditor(IEditor $directEditor): void {
+		$this->editors[$directEditor->getId()] = $directEditor;
+	}
+
+	public function getEditors(): array {
+		return $this->editors;
+	}
+
+	public function getTemplates(string $editor, string $type): array {
+		if (!array_key_exists($editor, $this->editors)) {
+			throw new \RuntimeException('No matching editor found');
+		}
+		$templates = [];
+		foreach ($this->editors[$editor]->getCreators() as $creator) {
+			if ($creator instanceof ICreateFromTemplate && $creator->getId() === $type) {
+				$templates = $creator->getTemplates();
+			}
+		}
+		return $templates;
+	}
+
+	public function create(string $path, string $editorId, string $creatorId, $templateId = null): string {
+		$userFolder = $this->rootFolder->getUserFolder($this->userId);
+		$file = $userFolder->newFile($path);
+		$editor = $this->getEditor($editorId);
+		$creators = $editor->getCreators();
+		foreach ($creators as $creator) {
+			if ($creator->getId() === $creatorId) {
+				$creator->create($file, $creatorId, $templateId);
+				return $this->createToken($editorId, $file);
+			}
+		}
+		throw new \RuntimeException('No creator found');
+	}
+
+	public function open(int $fileId, string $editorId = null): string {
+		$file = $this->rootFolder->getUserFolder($this->userId)->getById($fileId);
+		if (count($file) === 0 || !($file[0] instanceof File) || $file === null) {
+			throw new NotFoundException();
+		}
+		/** @var File $file */
+		$file = $file[0];
+
+		if ($editorId === null) {
+			$editorId = $this->findEditorForFile($file);
+		}
+
+		return $this->createToken($editorId, $file);
+	}
+
+	private function findEditorForFile(File $file) {
+		foreach ($this->editors as $editor) {
+			if (in_array($file->getMimeType(), $editor->getMimetypes())) {
+				return $editor->getId();
+			}
+		}
+		throw new \RuntimeException('No default editor found for files mimetype');
+	}
+
+	public function edit(string $token): Response {
+		try {
+			/** @var IEditor $editor */
+			$tokenObject = $this->getToken($token);
+			// TODO: REMOVE DEBUG CHECK BEFORE MERGING
+			if ($tokenObject->hasBeenAccessed() && !\OC::$server->getSystemConfig()->getValue('debug', false)) {
+				throw new \RuntimeException('Token has already been used and can only be used for followup requests');
+			}
+			$editor = $this->getEditor($tokenObject->getEditor());
+			$this->accessToken($token);
+
+		} catch (\Throwable $throwable) {
+			// TODO: if file gets accessed twice the token will be invalidated in general
+			$this->invalidateToken($token);
+			return new NotFoundResponse();
+		}
+		return $editor->open($tokenObject);
+	}
+
+	public function editSecure(File $file, string $editorId): TemplateResponse {
+		// TODO: Implementation in follow up
+	}
+
+	private function getEditor($editorId): IEditor {
+		if (!array_key_exists($editorId, $this->editors)) {
+			throw new \RuntimeException('No editor found');
+		}
+		return $this->editors[$editorId];
+	}
+
+	public function getToken(string $token): IToken {
+		$query = $this->connection->getQueryBuilder();
+		$query->select('*')->from(self::TABLE_TOKENS)
+			->where($query->expr()->eq('token', $query->createNamedParameter($token, IQueryBuilder::PARAM_STR)));
+		$result = $query->execute();
+		if ($result) {
+			return new Token($this, $result->fetch(FetchMode::ASSOCIATIVE));
+		}
+		throw new \RuntimeException('Failed to validate the token');
+	}
+
+	public function cleanup(): void {
+		$query = $this->connection->getQueryBuilder();
+		$query->delete(self::TABLE_TOKENS)
+			->where($query->expr()->lt('timestamp', $query->createNamedParameter(time() - 300)));
+		$result = $query->execute();
+	}
+
+	public function refreshToken(string $token): bool {
+		$query = $this->connection->getQueryBuilder();
+		$query->update(self::TABLE_TOKENS)
+			->set('timestamp', $query->createNamedParameter(time(), IQueryBuilder::PARAM_INT))
+			->where($query->expr()->eq('token', $query->createNamedParameter($token, IQueryBuilder::PARAM_STR)));
+		$result = $query->execute();
+		return $result !== 0;
+	}
+
+
+	public function invalidateToken(string $token): bool {
+		$query = $this->connection->getQueryBuilder();
+		$query->delete(self::TABLE_TOKENS)
+			->where($query->expr()->eq('token', $query->createNamedParameter($token, IQueryBuilder::PARAM_STR)));
+		$result = $query->execute();
+		return $result !== 0;
+	}
+
+	public function accessToken(string $token): bool {
+		$query = $this->connection->getQueryBuilder();
+		$query->update(self::TABLE_TOKENS)
+			->set('accessed', $query->createNamedParameter(true, IQueryBuilder::PARAM_BOOL))
+			->set('timestamp', $query->createNamedParameter(time(), IQueryBuilder::PARAM_INT))
+			->where($query->expr()->eq('token', $query->createNamedParameter($token, IQueryBuilder::PARAM_STR)));
+		$result = $query->execute();
+		return $result !== 0;
+	}
+
+	public function invokeTokenScope($userId): void {
+		\OC_User::setIncognitoMode(true);
+		\OC_User::setUserId($userId);
+	}
+
+	public function createToken($editorId, File $file, IShare $share = null): string {
+		$token = $this->random->generate(64, ISecureRandom::CHAR_HUMAN_READABLE);
+		$query = $this->connection->getQueryBuilder();
+		$query->insert(self::TABLE_TOKENS)
+			->values([
+				'token' => $query->createNamedParameter($token),
+				'editor_id' => $query->createNamedParameter($editorId),
+				'file_id' => $query->createNamedParameter($file->getId()),
+				'user_id' => $query->createNamedParameter($this->userId),
+				'share_id' => $query->createNamedParameter($share !== null ? $share->getId(): null),
+				'timestamp' => $query->createNamedParameter(time())
+			]);
+		$query->execute();
+		return $token;
+	}
+
+	public function getFileForToken($userId, $fileId) {
+		$userFolder = $this->rootFolder->getUserFolder($userId);
+		return $userFolder->getById($fileId)[0];
+	}
+
+}

xynm/nextcloud-server

lib/composer/composer/autoload_static.php
@@ -199,6 +199,13 @@ class ComposerStaticInit53792487c5a8370acc0b06b1a864ff4c
         'OCP\\Diagnostics\\IEventLogger' => __DIR__ . '/../../..' . '/lib/public/Diagnostics/IEventLogger.php',
         'OCP\\Diagnostics\\IQuery' => __DIR__ . '/../../..' . '/lib/public/Diagnostics/IQuery.php',
         'OCP\\Diagnostics\\IQueryLogger' => __DIR__ . '/../../..' . '/lib/public/Diagnostics/IQueryLogger.php',
+        'OCP\\DirectEditing\\ACreateEmpty' => __DIR__ . '/../../..' . '/lib/public/DirectEditing/ACreateEmpty.php',
+        'OCP\\DirectEditing\\ACreateFromTemplate' => __DIR__ . '/../../..' . '/lib/public/DirectEditing/ACreateFromTemplate.php',
+        'OCP\\DirectEditing\\ATemplate' => __DIR__ . '/../../..' . '/lib/public/DirectEditing/ATemplate.php',
+        'OCP\\DirectEditing\\IEditor' => __DIR__ . '/../../..' . '/lib/public/DirectEditing/IEditor.php',
+        'OCP\\DirectEditing\\IManager' => __DIR__ . '/../../..' . '/lib/public/DirectEditing/IManager.php',
+        'OCP\\DirectEditing\\IToken' => __DIR__ . '/../../..' . '/lib/public/DirectEditing/IToken.php',
+        'OCP\\DirectEditing\\RegisterDirectEditorEvent' => __DIR__ . '/../../..' . '/lib/public/DirectEditing/RegisterDirectEditorEvent.php',
         'OCP\\Encryption\\Exceptions\\GenericEncryptionException' => __DIR__ . '/../../..' . '/lib/public/Encryption/Exceptions/GenericEncryptionException.php',
         'OCP\\Encryption\\IEncryptionModule' => __DIR__ . '/../../..' . '/lib/public/Encryption/IEncryptionModule.php',
         'OCP\\Encryption\\IFile' => __DIR__ . '/../../..' . '/lib/public/Encryption/IFile.php',
@@ -815,6 +822,7 @@ class ComposerStaticInit53792487c5a8370acc0b06b1a864ff4c
         'OC\\Core\\Migrations\\Version16000Date20190428150708' => __DIR__ . '/../../..' . '/core/Migrations/Version16000Date20190428150708.php',
         'OC\\Core\\Migrations\\Version17000Date20190514105811' => __DIR__ . '/../../..' . '/core/Migrations/Version17000Date20190514105811.php',
         'OC\\Core\\Migrations\\Version18000Date20190920085628' => __DIR__ . '/../../..' . '/core/Migrations/Version18000Date20190920085628.php',
+        'OC\\Core\\Migrations\\Version18000Date20191014105105' => __DIR__ . '/../../..' . '/core/Migrations/Version18000Date20191014105105.php',
         'OC\\Core\\Notification\\RemoveLinkSharesNotifier' => __DIR__ . '/../../..' . '/core/Notification/RemoveLinkSharesNotifier.php',
         'OC\\Core\\Service\\LoginFlowV2Service' => __DIR__ . '/../../..' . '/core/Service/LoginFlowV2Service.php',
         'OC\\DB\\Adapter' => __DIR__ . '/../../..' . '/lib/private/DB/Adapter.php',
@@ -866,6 +874,8 @@ class ComposerStaticInit53792487c5a8370acc0b06b1a864ff4c
         'OC\\Diagnostics\\EventLogger' => __DIR__ . '/../../..' . '/lib/private/Diagnostics/EventLogger.php',
         'OC\\Diagnostics\\Query' => __DIR__ . '/../../..' . '/lib/private/Diagnostics/Query.php',
         'OC\\Diagnostics\\QueryLogger' => __DIR__ . '/../../..' . '/lib/private/Diagnostics/QueryLogger.php',
+        'OC\\DirectEditing\\Manager' => __DIR__ . '/../../..' . '/lib/private/DirectEditing/Manager.php',
+        'OC\\DirectEditing\\Token' => __DIR__ . '/../../..' . '/lib/private/DirectEditing/Token.php',
         'OC\\Encryption\\DecryptAll' => __DIR__ . '/../../..' . '/lib/private/Encryption/DecryptAll.php',
         'OC\\Encryption\\EncryptionWrapper' => __DIR__ . '/../../..' . '/lib/private/Encryption/EncryptionWrapper.php',
         'OC\\Encryption\\Exceptions\\DecryptionFailedException' => __DIR__ . '/../../..' . '/lib/private/Encryption/Exceptions/DecryptionFailedException.php',

lib/private/DirectEditing/Manager.php
@@ -1,0 +1,232 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019 Julius Härtl <jus@bitgrid.net>
+ *
+ * @author Julius Härtl <jus@bitgrid.net>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OC\DirectEditing;
+
+use Doctrine\DBAL\FetchMode;
+use OCP\AppFramework\Http\NotFoundResponse;
+use OCP\AppFramework\Http\Response;
+use OCP\AppFramework\Http\TemplateResponse;
+use OCP\DB\QueryBuilder\IQueryBuilder;
+use OCP\DirectEditing\ACreateFromTemplate;
+use OCP\DirectEditing\IEditor;
+use \OCP\DirectEditing\IManager;
+use OCP\DirectEditing\IToken;
+use OCP\Files\File;
+use OCP\Files\IRootFolder;
+use OCP\Files\NotFoundException;
+use OCP\IDBConnection;
+use OCP\IUserSession;
+use OCP\Security\ISecureRandom;
+use OCP\Share\IShare;
+
+class Manager implements IManager {
+
+	private const TOKEN_CLEANUP_TIME = 12 * 60 * 60 ;
+
+	public const TABLE_TOKENS = 'direct_edit';
+
+	/** @var IEditor[] */
+	private $editors;
+
+	/** @var IDBConnection */
+	private $connection;
+	/**
+	 * @var ISecureRandom
+	 */
+	private $random;
+	private $userId;
+	private $rootFolder;
+
+	public function __construct(
+		ISecureRandom $random,
+		IDBConnection $connection,
+		IUserSession $userSession,
+		IRootFolder $rootFolder
+	) {
+		$this->random = $random;
+		$this->connection = $connection;
+		$this->userId = $userSession->getUser() ? $userSession->getUser()->getUID() : null;
+		$this->rootFolder = $rootFolder;
+	}
+
+	public function registerDirectEditor(IEditor $directEditor): void {
+		$this->editors[$directEditor->getId()] = $directEditor;
+	}
+
+	public function getEditors(): array {
+		return $this->editors;
+	}
+
+	public function getTemplates(string $editor, string $type): array {
+		if (!array_key_exists($editor, $this->editors)) {
+			throw new \RuntimeException('No matching editor found');
+		}
+		$templates = [];
+		foreach ($this->editors[$editor]->getCreators() as $creator) {
+			if ($creator instanceof ACreateFromTemplate && $creator->getId() === $type) {
+				$templates = $creator->getTemplates();
+			}
+		}
+		return $templates;
+	}
+
+	public function create(string $path, string $editorId, string $creatorId, $templateId = null): string {
+		$userFolder = $this->rootFolder->getUserFolder($this->userId);
+		$file = $userFolder->newFile($path);
+		$editor = $this->getEditor($editorId);
+		$creators = $editor->getCreators();
+		foreach ($creators as $creator) {
+			if ($creator->getId() === $creatorId) {
+				$creator->create($file, $creatorId, $templateId);
+				return $this->createToken($editorId, $file);
+			}
+		}
+		throw new \RuntimeException('No creator found');
+	}
+
+	public function open(int $fileId, string $editorId = null): string {
+		$file = $this->rootFolder->getUserFolder($this->userId)->getById($fileId);
+		if (count($file) === 0 || !($file[0] instanceof File) || $file === null) {
+			throw new NotFoundException();
+		}
+		/** @var File $file */
+		$file = $file[0];
+
+		if ($editorId === null) {
+			$editorId = $this->findEditorForFile($file);
+		}
+
+		return $this->createToken($editorId, $file);
+	}
+
+	private function findEditorForFile(File $file) {
+		foreach ($this->editors as $editor) {
+			if (in_array($file->getMimeType(), $editor->getMimetypes())) {
+				return $editor->getId();
+			}
+		}
+		throw new \RuntimeException('No default editor found for files mimetype');
+	}
+
+	public function edit(string $token): Response {
+		try {
+			/** @var IEditor $editor */
+			$tokenObject = $this->getToken($token);
+			if ($tokenObject->hasBeenAccessed()) {
+				throw new \RuntimeException('Token has already been used and can only be used for followup requests');
+			}
+			$editor = $this->getEditor($tokenObject->getEditor());
+			$this->accessToken($token);
+
+		} catch (\Throwable $throwable) {
+			$this->invalidateToken($token);
+			return new NotFoundResponse();
+		}
+		return $editor->open($tokenObject);
+	}
+
+	public function editSecure(File $file, string $editorId): TemplateResponse {
+		// TODO: Implementation in follow up
+	}
+
+	private function getEditor($editorId): IEditor {
+		if (!array_key_exists($editorId, $this->editors)) {
+			throw new \RuntimeException('No editor found');
+		}
+		return $this->editors[$editorId];
+	}
+
+	public function getToken(string $token): IToken {
+		$query = $this->connection->getQueryBuilder();
+		$query->select('*')->from(self::TABLE_TOKENS)
+			->where($query->expr()->eq('token', $query->createNamedParameter($token, IQueryBuilder::PARAM_STR)));
+		$result = $query->execute();
+		if ($tokenRow = $result->fetch(FetchMode::ASSOCIATIVE)) {
+			return new Token($this, $tokenRow);
+		}
+		throw new \RuntimeException('Failed to validate the token');
+	}
+
+	public function cleanup(): int {
+		$query = $this->connection->getQueryBuilder();
+		$query->delete(self::TABLE_TOKENS)
+			->where($query->expr()->lt('timestamp', $query->createNamedParameter(time() - self::TOKEN_CLEANUP_TIME)));
+		return $query->execute();
+	}
+
+	public function refreshToken(string $token): bool {
+		$query = $this->connection->getQueryBuilder();
+		$query->update(self::TABLE_TOKENS)
+			->set('timestamp', $query->createNamedParameter(time(), IQueryBuilder::PARAM_INT))
+			->where($query->expr()->eq('token', $query->createNamedParameter($token, IQueryBuilder::PARAM_STR)));
+		$result = $query->execute();
+		return $result !== 0;
+	}
+
+
+	public function invalidateToken(string $token): bool {
+		$query = $this->connection->getQueryBuilder();
+		$query->delete(self::TABLE_TOKENS)
+			->where($query->expr()->eq('token', $query->createNamedParameter($token, IQueryBuilder::PARAM_STR)));
+		$result = $query->execute();
+		return $result !== 0;
+	}
+
+	public function accessToken(string $token): bool {
+		$query = $this->connection->getQueryBuilder();
+		$query->update(self::TABLE_TOKENS)
+			->set('accessed', $query->createNamedParameter(true, IQueryBuilder::PARAM_BOOL))
+			->set('timestamp', $query->createNamedParameter(time(), IQueryBuilder::PARAM_INT))
+			->where($query->expr()->eq('token', $query->createNamedParameter($token, IQueryBuilder::PARAM_STR)));
+		$result = $query->execute();
+		return $result !== 0;
+	}
+
+	public function invokeTokenScope($userId): void {
+		\OC_User::setIncognitoMode(true);
+		\OC_User::setUserId($userId);
+	}
+
+	public function createToken($editorId, File $file, IShare $share = null): string {
+		$token = $this->random->generate(64, ISecureRandom::CHAR_HUMAN_READABLE);
+		$query = $this->connection->getQueryBuilder();
+		$query->insert(self::TABLE_TOKENS)
+			->values([
+				'token' => $query->createNamedParameter($token),
+				'editor_id' => $query->createNamedParameter($editorId),
+				'file_id' => $query->createNamedParameter($file->getId()),
+				'user_id' => $query->createNamedParameter($this->userId),
+				'share_id' => $query->createNamedParameter($share !== null ? $share->getId(): null),
+				'timestamp' => $query->createNamedParameter(time())
+			]);
+		$query->execute();
+		return $token;
+	}
+
+	public function getFileForToken($userId, $fileId) {
+		$userFolder = $this->rootFolder->getUserFolder($userId);
+		return $userFolder->getById($fileId)[0];
+	}
+
+}